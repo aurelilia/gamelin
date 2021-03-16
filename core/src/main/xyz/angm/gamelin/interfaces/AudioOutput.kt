e6576e5cc05e72664ff91a8cb0d83984e6aef4cd
Ellie Ang <git@angm.xyz>
2021-03-16 11:30:41 +0100

    Refactor AudioOutput

--- core/src/main/xyz/angm/gamelin/interfaces/AudioOutput.kt
+++ core/src/main/xyz/angm/gamelin/interfaces/AudioOutput.kt
@@ -1,6 +1,6 @@
 /*
  * Developed as part of the Gamelin project.
- * This file was last modified at 3/16/21, 11:01 AM.
+ * This file was last modified at 3/16/21, 11:28 AM.
  * Copyright 2021, see git repository at git.angm.xyz for authors and other info.
  * This file is under the GPL3 license. See LICENSE in the root directory of this repository for details.
  */
@@ -8,38 +8,28 @@
 package xyz.angm.gamelin.interfaces
 
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.audio.AudioDevice
-import com.badlogic.gdx.utils.GdxRuntimeException
 import xyz.angm.gamelin.system.CLOCK_SPEED_HZ
 
 class AudioOutput {
 
-    private val SAMPLE_RATE = 22050
-    private val BUFFER_SIZE = 1024
-
-    private var device: AudioDevice? = null
+    private var device = Gdx.audio.newAudioDevice(SAMPLE_RATE, false)
     private var counter = 0
-    private var divider = CLOCK_SPEED_HZ / SAMPLE_RATE
     private var bufferIndex = 0
     private val buffer = ShortArray(BUFFER_SIZE)
-    private var audioThread: AudioThread? = null
-    val str = StringBuilder()
+
+    @Volatile
+    var play = false
+        @Synchronized set
 
     init {
-        initialize()
-    }
-
-    fun initialize() {
-        device = try {
-            Gdx.audio.newAudioDevice(SAMPLE_RATE, false)
-        } catch (e: GdxRuntimeException) {
-            null
-        }
-
-        if (device != null) {
-            audioThread = AudioThread(device!!)
-            audioThread!!.start()
-        }
+        Thread {
+            while (true) {
+                if (play) {
+                    device.writeSamples(buffer, 0, buffer.size)
+                    play = false
+                }
+            }
+        }.start()
     }
 
     fun reset() {
@@ -50,43 +40,23 @@ class AudioOutput {
 
     fun play(left: Byte, right: Byte) {
         if (counter++ != 0) {
-            counter %= divider
+            counter %= DIVIDER
             return
         }
-        if (left != 0.toByte()) {
-            str.appendLine(left)
-        }
 
-        buffer[bufferIndex++] = (left * 25).toShort()
-        buffer[bufferIndex++] = (right * 25).toShort()
-
+        buffer[bufferIndex++] = (left * 16).toShort()
+        buffer[bufferIndex++] = (right * 16).toShort()
         if (bufferIndex >= BUFFER_SIZE) {
-            audioThread?.buffer = buffer
-            audioThread?.play = true
-
+            play = true
             bufferIndex = 0
         }
     }
 
-    fun dispose() {
-        Gdx.files.local("sound").writeString(str.toString(), false)
-        device?.dispose()
-    }
-}
+    fun dispose() = device.dispose()
 
-class AudioThread(private val device: AudioDevice) : Thread() {
-
-    @Volatile
-    var play = false
-        @Synchronized set
-    var buffer = ShortArray(1024)
-
-    override fun run() {
-        while (true) {
-            if (play) {
-                device.writeSamples(buffer, 0, 1024)
-                play = false
-            }
-        }
+    companion object {
+        private const val SAMPLE_RATE = 22050
+        private const val BUFFER_SIZE = (2048 * (SAMPLE_RATE / 22050f)).toInt()
+        private const val DIVIDER = CLOCK_SPEED_HZ / SAMPLE_RATE
     }
 }
