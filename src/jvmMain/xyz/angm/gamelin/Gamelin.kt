3dca49edc618092852fc21856a64802a974e5b69
Ellie Ang <git@angm.xyz>
2021-03-24 00:32:26 +0100

    Hopefully fix libGDX native code crashes by waiting for GB to stop fully after dispose

--- src/jvmMain/xyz/angm/gamelin/Gamelin.kt
+++ src/jvmMain/xyz/angm/gamelin/Gamelin.kt
@@ -1,6 +1,6 @@
 /*
  * Developed as part of the Gamelin project.
- * This file was last modified at 3/21/21, 7:38 PM.
+ * This file was last modified at 3/24/21, 12:30 AM.
  * Copyright 2021, see git repository at git.angm.xyz for authors and other info.
  * This file is under the GPL3 license. See LICENSE in the root directory of this repository for details.
  */
@@ -176,6 +176,7 @@ class Gamelin : ApplicationAdapter() {
     override fun dispose() {
         saveConfiguration()
         gb.dispose()
+        Thread.sleep(20)
         stage.dispose()
         exitProcess(0)
     }
