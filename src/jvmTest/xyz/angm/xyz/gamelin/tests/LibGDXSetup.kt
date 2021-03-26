e1973915078bfbf707e233c480912bd09efa9e3c
Ellie Ang <git@angm.xyz>
2021-03-26 23:36:42 +0100

    Fix build failure...

--- src/jvmTest/xyz/angm/xyz/gamelin/tests/LibGDXSetup.kt
+++ src/jvmTest/xyz/angm/xyz/gamelin/tests/LibGDXSetup.kt
@@ -1,6 +1,6 @@
 /*
  * Developed as part of the Gamelin project.
- * This file was last modified at 3/26/21, 3:50 PM.
+ * This file was last modified at 3/26/21, 11:35 PM.
  * Copyright 2021, see git repository at git.angm.xyz for authors and other info.
  * This file is under the GPL3 license. See LICENSE in the root directory of this repository for details.
  */
@@ -11,16 +11,11 @@ import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.backends.headless.HeadlessApplication
 import io.kotest.core.listeners.ProjectListener
 import io.kotest.core.spec.AutoScan
-import xyz.angm.gamelin.configuration
 
 @AutoScan
 @Suppress("unused")
 object LibGDXSetup : ProjectListener {
     override suspend fun beforeProject() {
-        // Ensure we're running tests in DMG mode, some mooneye and blargg tests require it
-        // and the bootrom takes less time
-        configuration.preferCGB = false
-
         // Initialize libGDX to allow the tests to use some of the
         // desktop frontend
         HeadlessApplication(object : ApplicationAdapter() {})
