4aaa07b34fd6dc0329e3b047aad84f59edeab625
Ellie Ang <git@angm.xyz>
2021-03-28 21:55:40 +0200

    Properly pad file chooser close button

--- src/jvmMain/xyz/angm/gamelin/Gamelin.kt
+++ src/jvmMain/xyz/angm/gamelin/Gamelin.kt
@@ -1,6 +1,6 @@
 /*
  * Developed as part of the Gamelin project.
- * This file was last modified at 3/28/21, 9:25 PM.
+ * This file was last modified at 3/28/21, 9:55 PM.
  * Copyright 2021, see git repository at git.angm.xyz for authors and other info.
  * This file is under the GPL3 license. See LICENSE in the root directory of this repository for details.
  */
@@ -205,6 +205,7 @@ class Gamelin : ApplicationAdapter() {
         FileChooser.setDefaultPrefsName("gamelin")
         FileChooser.setSaveLastDirectory(true)
         val chooser = FileChooser("Choose ROM", FileChooser.Mode.OPEN)
+        Window.padCloseButton(chooser)
         chooser.selectionMode = FileChooser.SelectionMode.FILES
         val filter = FileTypeFilter(true)
         filter.addRule("GameBoy ROMs (.gb, .gbc)", "gb", "gbc")
--- src/jvmMain/xyz/angm/gamelin/windows/Window.kt
+++ src/jvmMain/xyz/angm/gamelin/windows/Window.kt
@@ -1,6 +1,6 @@
 /*
  * Developed as part of the Gamelin project.
- * This file was last modified at 3/28/21, 5:55 PM.
+ * This file was last modified at 3/28/21, 9:54 PM.
  * Copyright 2021, see git repository at git.angm.xyz for authors and other info.
  * This file is under the GPL3 license. See LICENSE in the root directory of this repository for details.
  */
@@ -18,10 +18,16 @@ abstract class Window(name: String, closable: Boolean = true) : VisWindow(name) 
 
     override fun addCloseButton() {
         super.addCloseButton()
+        padCloseButton(this)
+    }
+
+    companion object {
         // Add a tiny bit of top padding to the close button. It covers part of the window
         // border when using the tinted skin otherwise
-        val cells = titleTable.cells
-        val cell = cells[cells.size - 1]
-        cell.padTop(1f)
+        fun padCloseButton(window: VisWindow) {
+            val cells = window.titleTable.cells
+            val cell = cells[cells.size - 1]
+            cell.padTop(1f)
+        }
     }
 }
